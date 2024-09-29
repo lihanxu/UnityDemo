c569214bb140cd31f8889dc0a1a9cab5eecb0b65
old face <53005384+lihanxu@users.noreply.github.com>
2024-09-30 00:54:07 +0800

    【添加】 实现跳跃
    实现跳跃脚本

--- UniSideGame/Assets/Player/PlayerController.cs
+++ UniSideGame/Assets/Player/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody;
     float axisH;
-    public float speed = 3;
+    public float speed = 3f;
+    public float jump = 9.0f;
+    public LayerMask groundMask;
+    bool onGround = false;
+    bool goJump = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        axisH = Input.GetAxis("Horizontal");
+        axisH = Input.GetAxisRaw("Horizontal");
         if (axisH > 0) {
             Debug.Log("right");
             transform.localScale = new Vector2(1, 1);
@@ -25,10 +29,27 @@ public class PlayerController : MonoBehaviour
             Debug.Log("left");
             transform.localScale = new Vector2(-1, 1);
         }
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
     }
 
     void FixedUpdate()
     {
-        rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundMask);
+        if (onGround || axisH != 0) {
+            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        }
+
+        if (onGround && goJump) {
+            Vector2 force = new(0, jump);
+            rbody.AddForce(force, ForceMode2D.Impulse);
+            goJump = false;
+        }
+    }
+
+    void Jump() {
+        goJump = true;
+        Debug.Log("按下了跳跃键");
     }
 }
