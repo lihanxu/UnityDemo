258bed555d3e68a65e004df280dc2e1dcf19ac0b
lihanxu <610586189@qq.com>
2024-09-30 16:55:07 +0800

    【添加】实现移动、跳、结束等动画

--- UniSideGame/Assets/Player/PlayerController.cs
+++ UniSideGame/Assets/Player/PlayerController.cs
@@ -11,16 +11,30 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundMask;
     bool onGround = false;
     bool goJump = false;
+    Animator anim;
+    public string stopAnime = "PlayerStop";
+    public string moveAnime = "PlayerMove";
+    public string jumpAnime = "PlayerJump";
+    public string goalAnime = "PlayerGoal";
+    public string deadAnime = "PlayerDead";
+    string nowAnime = "PlayerStop";
+    string oldAnime = "PlayerStop";
+    public static string gameState = "playing";
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = this.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        gameState = "playing";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameState != "playing") {
+            return;
+        }
         axisH = Input.GetAxisRaw("Horizontal");
         if (axisH > 0) {
             Debug.Log("right");
@@ -36,6 +50,9 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (gameState != "playing") {
+            return;
+        }
         onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundMask);
         if (onGround || axisH != 0) {
             rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
@@ -46,10 +63,48 @@ public class PlayerController : MonoBehaviour
             rbody.AddForce(force, ForceMode2D.Impulse);
             goJump = false;
         }
+        if (onGround) {
+            nowAnime = axisH == 0 ? stopAnime : moveAnime;
+        } else {
+            nowAnime = jumpAnime;
+        }
+        if (nowAnime != oldAnime) {
+            anim.Play(nowAnime);
+            oldAnime = nowAnime;
+        }
     }
 
     void Jump() {
         goJump = true;
         Debug.Log("按下了跳跃键");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Goal") {
+            GameGoal();
+        } else if (other.gameObject.tag == "Dead") {
+            GameOver();
+        }
+    }
+
+    void GameOver() {
+        Debug.Log("游戏结束");
+        anim.Play(deadAnime);
+        gameState = "gameover";
+        GameStop();
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+
+    void GameGoal() {
+        Debug.Log("游戏胜利");
+        anim.Play(goalAnime);
+        gameState = "gameclear";
+        GameStop();
+    }
+
+    void GameStop() {
+        Debug.Log("停止游戏");
+        rbody.velocity = new Vector2(0, 0);
     }
 }
